7f48043272a458fbeb52c097e5bf7238ce4bdb72
kc <kc713@naver.com>
2022-10-24 11:17:41 +0900

    INIT

---
+++ src/main/java/com/ssafy/BravoSilverLife/BravoSilverLifeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.BravoSilverLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BravoSilverLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BravoSilverLifeApplication.class, args);
+	}
+
+}
