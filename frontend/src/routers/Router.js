f9dd73c252c2b46f5973d257ff284a47d892ff15
DESKTOP-KVCQHCD\multicampus <rdg9074@naver.com>
2022-10-24 15:26:17 +0900

    fix: file architecture edit

--- frontend/src/routers/Router.js
+++ frontend/src/routers/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+import MainNavBar from "../components/common/MainNavBar.js"
 
 function router() {
   return (
     <>
-      {/* <MainNavBar /> */}
+      <MainNavBar />
       <div id="router">
+        <div>hi</div>
         <Suspense>
           <Routes>
             <Route>
