db69a51632625102eb891e93f292d208e4806b94
DESKTOP-KVCQHCD\multicampus <rdg9074@naver.com>
2022-10-27 15:54:23 +0900

    feat: ranking-page edit

---
+++ frontend/src/components/BrandCard.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./BrandCard.css";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import DummyImg from "../assets/images/dummy-img.png";
+
+function BrandCard(props) {
+  return (
+    <div id="brand-card">
+      <div className="ranking-banner">
+        <img src="" alt="" className="ranking-banner__img" />
+        <p>ranking</p>
+      </div>
+      <div className="card-list">
+        <Row xs={1} md={2} className="g-4">
+          {Array.from({ length: 4 }).map((_, idx) => (
+            <Col>
+              <Card>
+                <Card.Img variant="top" src={DummyImg} />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </div>
+    </div>
+  );
+}
+
+export default BrandCard;
