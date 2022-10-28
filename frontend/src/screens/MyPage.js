9d17cc4925065958e840e9ccf20425f295ba968e
DESKTOP-KVCQHCD\multicampus <rdg9074@naver.com>
2022-10-28 17:28:12 +0900

    feat: mypage edit

--- frontend/src/screens/MyPage.js
+++ frontend/src/screens/MyPage.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MyPage.css";
+import Profile from "../components/Profile.js";
+import Like from "../components/Like.js";
+import BookMark from "../components/BookMark.js";
 
 function MyPage(props) {
+  const [category, setCategory] = useState("profile");
+
   return (
     <div id="my-page">
       <p align="center">mypage</p>
@@ -17,7 +22,9 @@ function MyPage(props) {
         </button>
       </div>
       <hr />
-      <div className="page-wrapper"></div>
+      <div className="page-wrapper">
+        <div>버튼별 컴포넌트 선택</div>
+      </div>
     </div>
   );
 }
