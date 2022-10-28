a13a4abe2de60c51abba4d8a7323488b6eee24f6
DESKTOP-KVCQHCD\multicampus <rdg9074@naver.com>
2022-10-28 16:19:05 +0900

    feat: profile-component

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Profile.css";
 
 function Profile(props) {
+  const gangnam = ["강남1", "강남2", "강남3", "강남4"];
+  const gangdong = ["강동1", "강동2", "강동3", "강동4"];
+  const gangbook = ["강북1", "강북2", "강북3", "강북4"];
+  const gangseo = ["강서1", "강서2", "강서3", "강서4"];
+
   return (
     <div id="profile">
       <div className="profile__info">
@@ -10,11 +15,28 @@ function Profile(props) {
         <p>이메일 : rdg9074@ssafy.com</p>
         <div className="profile__region">
           <p>관심지역</p>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              지역선택하기
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item value="강남">강남</Dropdown.Item>
+              <Dropdown.Item value="강동">강동</Dropdown.Item>
+              <Dropdown.Item value="강북">강북</Dropdown.Item>
+              <Dropdown.Item value="강서">강서</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
           <div className="profile__region-btn"></div>
         </div>
         <div className="profile__budget">
           <p>예산계획</p>
-          <div className="profile__budget-btn"></div>
+          <div className="profile__budget-btn">
+            <button type="button">5천 이내</button>
+            <button type="button">5천 ~ 1억</button>
+            <button type="button">1억 ~ 1억 5천</button>
+            <button type="button">1억 5천 ~ 2억</button>
+          </div>
         </div>
         <div className="profile__sector">
           <p>관심업종</p>
