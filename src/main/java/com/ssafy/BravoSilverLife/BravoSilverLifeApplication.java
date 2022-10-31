866deeccffaabc5fa3bdb9826cc2131f5ed41d4c
kc <kc713@naver.com>
2022-10-31 15:49:02 +0900

    Backend #feat: 구별 업종 검색 구현

--- src/main/java/com/ssafy/BravoSilverLife/BravoSilverLifeApplication.java
+++ src/main/java/com/ssafy/BravoSilverLife/BravoSilverLifeApplication.java
@@ -2,13 +2,24 @@ package com.ssafy.BravoSilverLife;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+//@PropertySource(value = {"classpath:database/database.properties"})
+//@PropertySource(value = {"classpath:oauth2/oauth2.properties"})
+//@PropertySource(value = {"classpath:swagger/springdoc.properties"})
+@EnableJpaAuditing
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@PropertySource(value = { "classpath:database/database.properties" })
+@PropertySource(value = { "classpath:oauth2/oauth2.properties" })
+@PropertySource(value = { "classpath:swagger/springdoc.properties" })
 public class BravoSilverLifeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BravoSilverLifeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BravoSilverLifeApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/ssafy/BravoSilverLife/controller/StoreController.java
@@ -0,0 +1,41 @@
+package com.ssafy.BravoSilverLife.controller;
+
+import com.ssafy.BravoSilverLife.dto.StoreDto;
+import com.ssafy.BravoSilverLife.entity.Store;
+import com.ssafy.BravoSilverLife.service.StoreService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Tag(name = "Store", description = "StoreAPI")
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/v1/store")
+public class StoreController {
+
+    @Autowired
+    StoreService storeService;
+
+    @Operation(summary = "구 상점 확인", description = "구이름으로 상점 확인하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공"),
+    })
+    @GetMapping("/popular")
+    public ResponseEntity getPopular(String gu,String category) throws Exception {
+        List<StoreDto> stores = storeService.getStores(gu,category);
+        return ResponseEntity.status(200).body(stores);
+
+
+    }
+}
