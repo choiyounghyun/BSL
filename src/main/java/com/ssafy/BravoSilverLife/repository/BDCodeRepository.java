6c068b61b425ee764704c94282838b19cee56910
kc <kc713@naver.com>
2022-11-01 16:00:33 +0900

    BackEnd #feat : 구, 동 검색

--- src/main/java/com/ssafy/BravoSilverLife/repository/BDCodeRepository.java
+++ src/main/java/com/ssafy/BravoSilverLife/repository/BDCodeRepository.java
@@ -3,6 +3,8 @@ package com.ssafy.BravoSilverLife.repository;
 import com.ssafy.BravoSilverLife.entity.BDCode;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,12 @@ import java.util.List;
 public interface BDCodeRepository extends JpaRepository<BDCode, Long> {
 
     List<BDCode> findByName(String Name);
+
+
+    @Query(value = "select distinct gugun from bd_code where sido =\"서울특별시\" and gugun is not null", nativeQuery = true)
+    List<String> findDistinctGugun();
+
+    @Query(value = "select name from bd_code where gugun =:gugun and name is not null", nativeQuery = true)
+    List<String> findDong(@Param("gugun") String gugun);
 
 }
