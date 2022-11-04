49c94a69e255b236199dea01877fc4119519296a
장우주 <ozoo109@naver.com>
2022-11-04 10:19:57 +0900

    api.js 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import Router from "./routers/Router";
 import "bootstrap/dist/css/bootstrap.css";
+import { useState } from "react";
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(false) // true 이면 로그인
+
   return (
     <Router />
   )
--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import "./Login.css"
 import { useState } from 'react'
-
-
+import api from "../Api"
 
 const Login = () => {
   const [id, setId] = useState("")
-
+  const loginUser = () => {
+    console.log("login");
+  };
   return (
     <div id='login-div'>
       <div className='container'>
-        <form>
+        <form onSubmit={loginUser}>
           <h1>로그인</h1>
           <span>아이디와 비밀번호를 입력해주세요</span>
           <input type="id" name="id" placeholder="아이디를 입력해주세요" />
           <input type="password" name="password" placeholder="비밀번호를 입력해주세요" />
-          <button type="">로그인</button>
+          <button type="submit">로그인</button>
           <button className='kakaologin'>카카오톡 로그인</button>
         </form>
       </div>
