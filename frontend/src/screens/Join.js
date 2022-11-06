6c0f7a70068e7839cc99ea3ebc67cd862164c337
장우주 <ozoo109@naver.com>
2022-11-07 08:51:25 +0900

    FE

--- frontend/src/screens/Join.js
+++ frontend/src/screens/Join.js
@@ -7,14 +7,12 @@ const Join = () => {
       <div>
         <h1>Bravo Silver Life</h1>
         <div>
-
-
           <input type="text" name="nickname" placeholder="닉네임을 입력해주세요" />
           <input type="id" name="id" placeholder="아이디를 입력해주세요" />
           <input type="password" name="password" placeholder="비밀번호를 입력해주세요" />
           <input type="email" name="email" placeholder="이메일을 입력해주세요" />
           <input type="number" name="phonenumber" placeholder="휴대전화번호를 입력해주세요" />
-          <button className="test" type="">인증번호받기</button>
+          <button className="test" >인증번호받기</button>
         </div>
         <button type="">회원가입</button>
       </div>
