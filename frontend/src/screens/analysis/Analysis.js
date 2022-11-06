e6b41802bc0de43c039966a89fbb8b7c0536fb04
ryh1999 <ryh1999@naver.com>
2022-11-07 02:04:33 +0900

    FE #feat: 디자인 변경중

--- frontend/src/screens/analysis/Analysis.js
+++ frontend/src/screens/analysis/Analysis.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import KakaoMap from './KakaoMap'
 import SideBar from './SideBar'
 import './Analysis.css'
+
+import Logo from '../../assets/AnalysisImages/BSL_Logo.png'
 
 const Analysis = () => { // 상권분석
 	// const [searchOption, setSearchOption] = useState({
@@ -11,6 +13,19 @@ const Analysis = () => { // 상권분석
 	// 	depositOrSaleMin: 0, depositOrSaleMax: 100000000, // 보증금 및 매매금 최소(0원), 최대(1조원)
 	// 	roomSizeMin: 0, roomSizeMax: 1000000, // 방 크기 최소(0㎡), 최대(1000000㎡)
 	// })
+	const [isLoading, setIsLoading] = useState(false)
+	useEffect(() => {
+		setTimeout(function () {
+			setIsLoading(true)
+		}, 4000)
+	})
+
+	const mainnavigate = useNavigate()
+	const gotoMain = () => {
+		mainnavigate("/")
+	}
+
+
 	const [place, setPlace] = useState('') // 지역(동) 및 역이름
 	const getPlaceName = (placeName) => { // 지역(동) 및 역이름을 정하는 함수
 		setPlace(placeName)
@@ -18,14 +33,26 @@ const Analysis = () => { // 상권분석
 
 	return (
 		// 상권분석
-		<div className="analysis_wrap">
-			{/* 지도 div */}
-			<KakaoMap className='map_wrap'
-				place={place} />
-			{/* 사이드바 div */}
-			<SideBar className='sidebar_wrap'
-				getPlaceName={getPlaceName} />
-		</div >
+		<>
+			{isLoading === false &&
+				<div className='loading_wrap flip'>
+					<img src={Logo} alt='Logo' />
+				</div>
+			}
+			{isLoading === true &&
+				<div className='analysis_wrap fade-in'>
+					<div className='go-to-main' onClick={gotoMain}>
+						<img src={Logo} alt='Logo' />
+					</div>
+					{/* 지도 div */}
+					<KakaoMap className='map_wrap'
+						place={place} />
+					{/* 사이드바 div */}
+					<SideBar className='sidebar_wrap'
+						getPlaceName={getPlaceName} />
+				</div>
+			}
+		</>
 	)
 }
 
--- frontend/src/screens/analysis/FindPlace.js
+++ frontend/src/screens/analysis/FindPlace.js
@@ -1,8 +1,40 @@
 import React from "react"
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs"
+import './FindPlace.css'
 
 const FindPlace = () => {
   return (
-    <div>조건 검색 창</div>
+    <div className="option_wrap">
+      <Tabs>
+        <TabList>
+          <Tab>
+            <p>업종</p>
+          </Tab>
+          <Tab>
+            <p>가격</p>
+          </Tab>
+          <Tab>
+            <p>크기 및 층수</p>
+          </Tab>
+        </TabList>
+
+        <TabPanel>
+          <div className="panel-content">
+            <h2>1</h2>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="panel-content">
+            <h2>2</h2>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="panel-content">
+            <h2>3</h2>
+          </div>
+        </TabPanel>
+      </Tabs>
+    </div>
   )
 }
 
