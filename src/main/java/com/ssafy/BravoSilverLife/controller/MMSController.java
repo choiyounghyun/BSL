8d3bab74cb84f13ade3121fe9aee10c92e00383d
kc <kc713@naver.com>
2022-11-08 10:11:32 +0900

    Backend #feat: 번호 중복 검사

--- src/main/java/com/ssafy/BravoSilverLife/controller/MMSController.java
+++ src/main/java/com/ssafy/BravoSilverLife/controller/MMSController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +21,11 @@ public class MMSController {
 
     @Operation(summary = "인증번호 발송 버튼", description = "인증번호 발송 & DB에 해당 핸드폰 번호에 인증번호 저장")
     @GetMapping("/check/{phone_number}")
-    public void MMSCheck(@PathVariable String phone_number){
+    public ResponseEntity MMSCheck(@PathVariable String phone_number){
         String access_token = mmsService.userAuth();
-        mmsService.checkAuthByMMS(access_token, phone_number);
+        boolean result = mmsService.checkAuthByMMS(access_token, phone_number);
+        if (result) return ResponseEntity.status(200).body("발송 완료");
+        else return ResponseEntity.status(200).body("중복 번호");
     }
 
 }
