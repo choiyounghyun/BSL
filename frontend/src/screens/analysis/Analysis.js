d1e7be9181754c2151d8dfc2078eae9c99a0af5c
ryh1999 <ryh1999@naver.com>
2022-11-09 08:32:11 +0900

    FE #feat: 매물 리스트 가져오기 구현

--- frontend/src/screens/analysis/Analysis.js
+++ frontend/src/screens/analysis/Analysis.js
@@ -12,20 +12,25 @@ const Analysis = () => { // 상권분석
 		place: '', sector: '', tradeType: 'all', floor: 'all',
 		monthly: [0, 100], deposit: [0, 100], sale: [0, 100], room: [0, 100]
 	})
-	const [latLngYX, setLatLngYX] = useState([0, 0, 0, 0])
+	const [mapAreaYX, setMapAreaYX] = useState([0, 0, 0, 0])
 	const [dongName, setDongName] = useState('')
 	const [clusterId, setClusterId] = useState(0)
 	const [clusterMaxPage, setClusterMaxPage] = useState(1)
+	const [clusterInfoList, setClusterInfoList] = useState('');
+	const [emptyStore, setEmptyStore] = useState('')
 
-	const getClusterDataList = () => {
+	useEffect(() => {
 		console.log(clusterMaxPage)
-		// if (dongName !== '' && markerId !== 0) {
-		// 	for (let i = 1; i <= clusterMaxPage; i++) {
-		// 		const clusterDataListURL = `https://k7c208.p.ssafy.io/api/v1/estate/articles?markerId=${markerId}&page=${i}&dongName=${dongName}&rentPriceMin=${optionDataList.monthly[0] * 10}&rentPriceMax=${optionDataList.monthly[1] * 10}&priceMin=${optionDataList.sale[0] * 100}&priceMax=${optionDataList.sale[1] * 100}&areaMin=${optionDataList.room[0] * 4}&areaMax=${optionDataList.room[1] * 4}&leftLon=${latLngYX[0]}&rightLon=${latLngYX[1]}&topLat=${latLngYX[2]}&bottomLat=${latLngYX[3]}`
-		// 		const response = await axios.get(clusterDataListURL)
-		// 		console.log(response.data.articles)
-		// 	}
-		// }
+		getEmptyStoreList()
+	}, [clusterId, clusterMaxPage])
+
+	async function getEmptyStoreList() {
+		for (let i = 1; i <= clusterMaxPage; i++) {
+			const emptyStoreListURL = `https://k7c208.p.ssafy.io/api/v1/estate/articles?markerId=${clusterId}&page=${i}&dongName=${dongName}&rentPriceMin=${optionDataList.monthly[0] * 10}&rentPriceMax=${optionDataList.monthly[1] * 10}&priceMin=${optionDataList.sale[0] * 100}&priceMax=${optionDataList.sale[1] * 100}&areaMin=${optionDataList.room[0] * 4}&areaMax=${optionDataList.room[1] * 4}&leftLon=${mapAreaYX[0]}&rightLon=${mapAreaYX[1]}&topLat=${mapAreaYX[2]}&bottomLat=${mapAreaYX[3]}`
+			const response = await axios.get(emptyStoreListURL)
+
+			console.log(response.data.articles)
+		}
 	}
 
 	const [isLoading, setIsLoading] = useState(false)
@@ -56,10 +61,9 @@ const Analysis = () => { // 상권분석
 					{/* 지도 div */}
 					<KakaoMap className='map_wrap'
 						optionDataList={optionDataList}
-						latLngYX={latLngYX} setLatLngYX={setLatLngYX}
-						dongName={dongName} setDongName={setDongName}
-						clusterId={clusterId} setClusterId={setClusterId}
-						getClusterDataList={getClusterDataList}
+						setDongName={setDongName}
+						setMapAreaYX={setMapAreaYX}
+						setClusterId={setClusterId}
 						setClusterMaxPage={setClusterMaxPage} />
 					{/* 사이드바 div */}
 					<SideBar className='sidebar_wrap'
