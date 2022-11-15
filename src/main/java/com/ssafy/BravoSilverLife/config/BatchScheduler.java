4874d3a2428a2c353d0aeeea9c7de98f980c5975
최영현 <chlasnmzx2@naver.com>
2022-11-15 20:56:34 +0900

    Update BatchScheduler.java

--- src/main/java/com/ssafy/BravoSilverLife/config/BatchScheduler.java
+++ src/main/java/com/ssafy/BravoSilverLife/config/BatchScheduler.java
@@ -24,7 +24,7 @@ public class BatchScheduler {
 
     private final BatchConfig batchConfig;
 
-    @Scheduled(cron = "* 53 20 * * *")
+    @Scheduled(cron = "0 0 21 * * *")
     public void runJob() {
 
         // job parameter 설정
